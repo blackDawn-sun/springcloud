2dc46184678507713e721d33850876ac45c13628
weixin_global <1171834530_global.qq.com>
2020-05-29 15:46:23 +0800

    //二维码生成工具

---
+++ eureka_client_1/src/main/java/com/example/eureka_client_1/service/QRCodeGenerator.java
@@ -0,0 +1,40 @@
+package com.example.eureka_client_1.service;
+
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+//二维码生成工具
+public class QRCodeGenerator {
+
+    private static final String QR_CODE_IMAGE_PATH = "e:MyQRCode.png";
+
+    private static void generateQRCodeImage(String text, int width, int height, String filePath) throws WriterException, IOException {
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+
+        BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, width, height);
+
+        Path path = FileSystems.getDefault().getPath(filePath);
+
+        MatrixToImageWriter.writeToPath(bitMatrix, "PNG", path);
+
+    }
+
+    public static void main(String[] args) {
+        try {
+            generateQRCodeImage("http://www.baidu.com", 350, 350, QR_CODE_IMAGE_PATH);
+        } catch (WriterException e) {
+            System.out.println("Could not generate QR Code, WriterException :: " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("Could not generate QR Code, IOException :: " + e.getMessage());
+        }
+
+    }
+
+
+}
